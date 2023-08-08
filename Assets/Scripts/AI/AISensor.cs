4945478e5b768a68d41d03def4ae20f07b057a6a
NAUL <nbluangamedev@gmail.com>
2023-08-08 18:11:47 +0700

    [add] tmp

---
+++ Assets/Scripts/AI/AISensor.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISensor : MonoBehaviour
+{
+    public float distance = 10f;
+    public float angle = 30f;
+    public float height = 1.0f;
+    public Color color = Color.red;
+
+    private Mesh mesh;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        int segment = 10;
+
+        int numTriangle = (segment * 4) + 2 + 2;
+        int numVertices = numTriangle * 3;
+
+        Vector3[] vertices = new Vector3[numVertices];
+        int[] triangles = new int[numVertices];
+
+        Vector3 bottomCenter = Vector3.zero;
+        Vector3 bottomLeft = Quaternion.Euler(0, -angle, 0) * Vector3.forward * distance;
+        Vector3 bottomRight = Quaternion.Euler(0, angle, 0) * Vector3.forward * distance;
+
+        Vector3 topCenter = bottomCenter + Vector3.up * height;
+        Vector3 topLeft = bottomLeft + Vector3.up * height;
+        Vector3 topRight = bottomRight + Vector3.up * height;
+
+        int vert = 0;
+
+        //left side
+        vertices[vert++] = bottomCenter;
+        vertices[vert++] = bottomLeft;
+        vertices[vert++] = topLeft;
+
+        vertices[vert++] = topLeft;
+        vertices[vert++] = topCenter;
+        vertices[vert++] = bottomCenter;
+
+        //right side
+        vertices[vert++] = bottomCenter;
+        vertices[vert++] = topCenter;
+        vertices[vert++] = topRight;
+
+        vertices[vert++] = topRight;
+        vertices[vert++] = bottomRight;
+        vertices[vert++] = bottomCenter;
+
+        float currentAngle = -angle;
+        float deltaAngle = (angle * 2) / segment;
+        for (int i = 0; i < segment; ++i)
+        {
+            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * distance;
+            bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward * distance;
+
+            topLeft = bottomLeft + Vector3.up * height;
+            topRight = bottomRight + Vector3.up * height;
+
+            //far
+            vertices[vert++] = bottomLeft;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = topRight;
+
+            vertices[vert++] = topRight;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = bottomLeft;
+
+            //top
+            vertices[vert++] = topCenter;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = topRight;
+
+            //bottom
+            vertices[vert++] = bottomCenter;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = bottomLeft;
+
+            currentAngle += deltaAngle;
+        }
+
+        for (int i = 0; i < numVertices; ++i)
+        {
+            triangles[i] = i;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    private void OnValidate()
+    {
+        mesh = CreateMesh();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (mesh)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
+        }
+    }
+}
